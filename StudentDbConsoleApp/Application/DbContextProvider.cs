c22957fbea5b934d8686148bce605e90b1dbcbab
Tushar Sharma <tusharsharma2509@gmail.com>
2024-08-03 02:56:24 +0530

    created db context provider

--- StudentDbConsoleApp/Application/DbContextProvider.cs
+++ StudentDbConsoleApp/Application/DbContextProvider.cs
@@ -1,7 +1,19 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using StudentDbConsoleApp.Infrastructure;
+
 namespace StudentDbConsoleApp.Application;
 
-internal class DbContextProvider
+internal class DbContextProvider : IDesignTimeDbContextFactory<AppDbContext>
 {
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(configuration["ConnectionStrings:StudentDb"]);
 
+        return new AppDbContext(optionsBuilder.Options);
+
+    }
 }
---
+++ StudentDbConsoleApp/Infrastructure/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDbConsoleApp.Infrastructure
+{
+    internal class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+            
+    }
+}
